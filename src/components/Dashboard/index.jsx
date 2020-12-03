fb408dc4f30fb4ec309a515941b0998b58d7af52
Kush Daga <kushdaga@hotmail.com>
2020-12-03 22:46:14 +0530

    Fixed loading and model

--- src/components/Dashboard/index.jsx
+++ src/components/Dashboard/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import Webcam from "react-webcam";
 import Result from "../Result";
 import * as faceapi from "face-api.js";
@@ -7,60 +7,86 @@ import * as faceapi from "face-api.js";
 export default function Dashboard() {
 	const webcamRef = useRef(null);
 	const [open, setOpen] = useState(false);
+	const [loading, setLoading] = useState(true);
+	const [loadingPredictions, setLoadingPredictions] = useState(true);
+	const [prediction, setPrediction] = useState(null);
 	// const { Canvas, Image, ImageData } = canvas;
 	// faceapi.env.monkeyPatch({ Canvas, Image, ImageData });
 
+	useEffect(() => {
+		const loadModels = async () => {
+			await faceapi.nets.ssdMobilenetv1.loadFromUri("/models");
+			await faceapi.nets.faceExpressionNet.loadFromUri("/models");
+			await faceapi.nets.tinyFaceDetector.loadFromUri("/models");
+			await faceapi.nets.ageGenderNet.loadFromUri("/models");
+			console.log("Models Loaded");
+			setLoading(false);
+		};
+		loadModels();
+	}, []);
+
 	const handleClick = async () => {
-		// setOpen(true);
+		setLoadingPredictions(true);
+		setOpen(true);
 		const imageSrc = webcamRef.current.getScreenshot();
 		const imgHTML = document.createElement("img");
 		imgHTML.src = imageSrc;
-		await faceapi.nets.ssdMobilenetv1.loadFromUri("/models");
-		await faceapi.nets.faceExpressionNet.loadFromUri("/models");
-		await faceapi.nets.tinyFaceDetector.loadFromUri("/models");
-
 		const detection = await faceapi
 			.detectAllFaces(imgHTML)
-			.withFaceExpressions();
-
+			.withFaceExpressions()
+			.withAgeAndGender();
+		setPrediction(detection);
+		setLoadingPredictions(false);
 		console.log(detection);
 	};
 
 	return (
-		<div className="w-full h-screen flex flex-col justify-center align-center bg-gray-100">
-			<nav className="fixed h-20 top-0 w-full bg-black flex align-center justify-between">
-				<p className="text-white text-lg ml-10 self-center">Welcome Kush</p>
-				<button className="text-black bg-gray-200 mr-10 self-center py-2 px-4 rounded-lg">
-					Logout
-				</button>
-			</nav>
-			<div className="flex-grow-0 w-auto max-w-screen-xl object-cover flex align-center justify-center w-100 bg-red-200 mx-auto">
-				<Webcam
-					audio={false}
-					height={1080}
-					ref={webcamRef}
-					screenshotFormat="image/jpeg"
-					width={1920}
-				/>
+		<div>
+			{loading ? (
+				"Models Loading"
+			) : (
+				<div className="w-full h-screen flex flex-col justify-center align-center bg-gray-100">
+					<nav className="fixed h-20 top-0 w-full bg-black flex align-center justify-between">
+						<p className="text-white text-lg ml-10 self-center">Welcome Kush</p>
+						<button className="text-black bg-gray-200 mr-10 self-center py-2 px-4 rounded-lg">
+							Logout
+						</button>
+					</nav>
+					<div className="flex-grow-0 w-auto max-w-screen-xl object-cover flex align-center justify-center w-100 bg-red-200 mx-auto">
+						<Webcam
+							audio={false}
+							height={1080}
+							ref={webcamRef}
+							screenshotFormat="image/jpeg"
+							width={1920}
+						/>
 
-				<div className="flex flex-col align-center justify-center p-4 ">
-					<h1 className="text-xl font-bold mb-3 ">Get started</h1>
-					<p className="text-md mb-5">
-						Welcome to your dashboard, please allow the permission for camera,
-						and let us take an image of your face after you are ready, click on
-						<span className="font-bold"> Get Recommendations</span> and after
-						that our software will return a list of Recommendations for you
-						based on your face emotion and mood.
-					</p>
-					<button
-						className="px-4 py-2 bg-black rounded-lg text-white"
-						onClick={handleClick}
-					>
-						Get Recommendations
-					</button>
+						<div className="flex flex-col align-center justify-center p-4 ">
+							<h1 className="text-xl font-bold mb-3 ">Get started</h1>
+							<p className="text-md mb-5">
+								Welcome to your dashboard, please allow the permission for
+								camera, and let us take an image of your face after you are
+								ready, click on
+								<span className="font-bold"> Get Recommendations</span> and
+								after that our software will return a list of Recommendations
+								for you based on your face emotion and mood.
+							</p>
+							<button
+								className="px-4 py-2 bg-black rounded-lg text-white"
+								onClick={handleClick}
+							>
+								Get Recommendations
+							</button>
+						</div>
+					</div>
+					<Result
+						open={open}
+						loading={loadingPredictions}
+						predictions={prediction}
+						setOpen={setOpen}
+					/>
 				</div>
-			</div>
-			<Result open={open} />
+			)}
 		</div>
 	);
 }
--- src/components/Result/index.jsx
+++ src/components/Result/index.jsx
@@ -1,13 +1,60 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-export default function Result({ open = false }) {
+export default function Result({
+	open = false,
+	loading,
+	predictions,
+	setOpen,
+}) {
+	const [noOfFaces, setNoOfFaces] = useState(0);
+	const [moods, setMoods] = useState([]);
+	useEffect(() => {
+		if (predictions !== null) {
+			setNoOfFaces(predictions.length);
+			predictions.forEach((prediction) => {
+				let maxMood = "";
+				let maxPercent = 0;
+				Object.keys(prediction.expressions).forEach((mood) => {
+					if (prediction.expressions[mood] > maxPercent) {
+						maxMood = mood;
+						maxPercent = prediction.expressions[mood];
+					}
+				});
+				setMoods((moods) => {
+					return [...moods, maxMood];
+				});
+			});
+		}
+	}, [predictions]);
+
 	return (
 		<div
 			className={`fixed shadow-xl h-3/4 w-3/4 bg-gray-300 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 transition-all ${
 				!open ? "hidden" : "block"
 			}`}
 		>
-			<div></div>
+			<button
+				onClick={() => {
+					setMoods([]);
+					setNoOfFaces(0);
+					setOpen(false);
+				}}
+			>
+				Close
+			</button>
+			<div>
+				{loading ? (
+					"Loading Data"
+				) : (
+					<div>
+						<div>
+							{moods?.map((mood, i) => (
+								<h2 key={i}>You seem {mood}</h2>
+							))}
+						</div>
+					</div>
+				)}
+			</div>
 		</div>
 	);
 }
